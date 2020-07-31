8fc3888e70094a896262c43c4c105e7edb10110e
siminski <laurensiminski@gmail.com>
2020-07-30 20:40:19 -0400

    inquirer prompting, instantiating, and pushing each new employee to an array

--- app.js
+++ app.js
@@ -10,9 +10,127 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+async function start() {
+	const employees = [];
+	
+	const manager = await addManager();
+	const newManager = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
+	employees.push(newManager);
+	
+	let selection = await selectType();
+	
+	while (selection.type !== 'I don\'t want to add any more team members') {
+		if (selection.type === 'Engineer') {
+			const engineer = await addEngineer();
+			const newEngineer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
+			employees.push(newEngineer);
+		} else {
+			const intern = await addIntern();
+			const newIntern = new Intern(intern.name, intern.id, intern.email, intern.school);
+			employees.push(newIntern);
+		}
+		selection = await selectType();
+	}
+	
+	console.log(employees);
+	//render(employees);
+
+}
+start();
+
+function addManager() {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your manager\'s name?'
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: 'What is your manager\'s id?'
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: 'What is your manager\'s email?'
+		},
+		{
+			type: 'input',
+			name: 'officeNumber',
+			message: 'What is your manager\'s office number?'
+		}
+	]);
+}
+
+function selectType() {
+	return inquirer.prompt([
+		{
+			type: 'list',
+			name: 'type',
+			message: 'What type of team member would you like to add?',
+			choices: [
+				'Engineer',
+				'Intern',
+				'I don\'t want to add any more team members'
+			]
+		}
+	]);
+}
+
+function addEngineer() {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your engineer\'s name?'
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: 'What is your engineer\'s id?'
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: 'What is your engineer\'s email?'
+		},
+		{
+			type: 'input',
+			name: 'github',
+			message: 'What is your engineer\'s GitHub username?'
+		}
+	]);
+}
+
+function addIntern() {
+	return inquirer.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: 'What is your intern\'s name?'
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: 'What is your intern\'s id?'
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: 'What is your intern\'s email?'
+		},
+		{
+			type: 'input',
+			name: 'school',
+			message: 'What is your intern\'s school?'
+		}
+	]);
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
